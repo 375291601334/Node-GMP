6d3be97fb2ca06d40b80e6e1589b9dc93dcdcfc4
Anastasiya Ramanava1 <Anastasiya_Ramanava1@epam.com>
2023-09-05 16:25:15 +0300

    feat: print result of command that depends on OS type

---
+++ activityMonitor.js
@@ -0,0 +1,26 @@
+const os = require('os');
+const childProcess = require('child_process');
+
+const command = getCommand();
+
+function getCommand() {
+  switch (os.type()) {
+    case 'Windows_NT':
+      return `powershell "Get-Process | Sort-Object CPU -Descending | Select-Object -Property Name, CPU, WorkingSet -First 1 | ForEach-Object { $_.Name + ' ' + $_.CPU + ' ' + $_.WorkingSet }"`;
+    case 'Darwin':
+    case 'Linux':
+      return `ps -A -o %cpu,%mem,comm | sort -nr | head -n 1`;
+  }
+}
+
+function execCommand() {
+  childProcess.exec(command, (error, stdout) => {
+    console.log(stdout);
+
+    if (error !== null) {
+      console.log(`error: ${error}`);
+    }
+  });
+}
+
+execCommand();
